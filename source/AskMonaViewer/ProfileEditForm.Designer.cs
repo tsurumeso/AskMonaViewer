c7c188a9d13075b6f56f536a1cefeaa714a3be2d
tsurumeso <nstm101339@gmail.com>
2017-08-08 11:42:58 +0900

    文字数制限を追加

--- source/AskMonaViewer/ProfileEditForm.Designer.cs
+++ source/AskMonaViewer/ProfileEditForm.Designer.cs
@@ -59,6 +59,7 @@
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.Location = new System.Drawing.Point(78, 12);
+            this.textBox1.MaxLength = 12;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(264, 23);
             this.textBox1.TabIndex = 2;
@@ -69,6 +70,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox2.Location = new System.Drawing.Point(78, 41);
+            this.textBox2.MaxLength = 255;
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(264, 127);
